6df40d4f635180576071bf7d9439e8004326907b
cynthia <feiver@naver.com>
2024-10-11 20:19:26 +0900

    닷지 공부

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody PlayerRigidbody; // 이동에 사용할 리디즈바디 컴포넌트
+    public Rigidbody playerRigidbody; // 이동에 사용할 리지드바디 컴포넌트
     public float speed = 8f; // 이동 속력
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,22 +19,22 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.UpArrow) == true)
         {
             // 위쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
-            playerRigibody.AddForce(0f, 0f, speed);
+            playerRigidbody.AddForce(0f, 0f, speed);
         }
         if (Input.GetKey(KeyCode.DownArrow) == true)
         {
             // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
-            playerRigibody.AddForce(0f, 0f, -speed);
+            playerRigidbody.AddForce(0f, 0f, -speed);
         }
         if (Input.GetKey(KeyCode.RightArrow) == true)
         {
             // 오른쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
-            playerRigibody.AddForce(speed, 0f, 0f);
+            playerRigidbody.AddForce(speed, 0f, 0f);
         }
         if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
             // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
-            playerRigibody.AddForce(-speed, 0f, 0f);
+            playerRigidbody.AddForce(-speed, 0f, 0f);
         }
     }
 
