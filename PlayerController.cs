2f4765e616677c11c40e4ba53db6cdf2d15f4350
cynthia <feiver@naver.com>
2024-10-11 20:12:13 +0900

    수정

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,14 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody PlayerRigidbody; // 이동에 사용할 리디즈바디 컴포넌트
     public float speed = 8f; // 이동 속력
-
-    // bool Input.GetKey(KeyCode key);
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
